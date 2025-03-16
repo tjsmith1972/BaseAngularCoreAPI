3a5bbc2f50bc919bab6d348ad0c293e7d58fbdfc
TJ Smith <tjsmith1972@gmail.com>
2025-03-16 07:12:26 -0400

    renamed solution

--- API/DTOs/UserDto.cs
+++ API/DTOs/UserDto.cs
@@ -8,5 +8,4 @@ public class UserDto
     public required string KnownAs { get; set; }
     public required string Token { get; set; }
     public required string Gender { get; set; }
-    public string? PhotoUrl { get; set; }
 }
